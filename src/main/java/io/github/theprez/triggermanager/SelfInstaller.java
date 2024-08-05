d9d6e10a80e92ce7e4a63c7150ab2818df72ac12
Dave Charron <david.s.charron@gmail.com>
2024-08-05 16:09:32 -0500

    Code cleanup and library qualfied CL commands

--- src/main/java/io/github/theprez/triggermanager/SelfInstaller.java
+++ src/main/java/io/github/theprez/triggermanager/SelfInstaller.java
@@ -5,33 +5,30 @@ import java.sql.SQLException;
 
 import com.github.theprez.jcmdutils.AppLogger;
 import com.ibm.as400.access.AS400;
-import com.ibm.as400.access.AS400SecurityException;
-import com.ibm.as400.access.CommandCall;
-import com.ibm.as400.access.ErrorCompletingRequestException;
 import com.ibm.as400.access.IFSFile;
 
 public class SelfInstaller {
-    private String m_library;
-    private AS400 m_as400;
-    private AppLogger m_logger;
+    private final String m_library;
+    private final AS400 m_as400;
+    private final AppLogger m_logger;
 
     public SelfInstaller(AppLogger _logger, AS400 _as400, String _library) {
         m_library = _library;
         m_as400 = _as400;
         m_logger = _logger;
     }
-    public void install() throws IOException, AS400SecurityException, ErrorCompletingRequestException, InterruptedException, SQLException {
+    public void install() throws IOException, SQLException {
         createLibraryIfNeeded();
     }
 
-
-    private void createLibraryIfNeeded() throws IOException, AS400SecurityException, ErrorCompletingRequestException, InterruptedException, SQLException {
-        IFSFile checker = new IFSFile(m_as400, "/qsys.lib/"+m_library+".lib");
+    private void createLibraryIfNeeded() throws IOException, SQLException {
+        IFSFile checker = new IFSFile(m_as400, "/qsys.lib/" + m_library + ".lib");
         if(checker.exists()) {
             m_logger.printfln_verbose("Library %s already exists", m_library);
+            return;
         }
         QCmdExc clCmdExc = new QCmdExc(m_logger, m_as400);
-        clCmdExc.execute("CRTLIB "+m_library);
+        clCmdExc.execute("QSYS/CRTLIB " + m_library);
     }
 
 }
