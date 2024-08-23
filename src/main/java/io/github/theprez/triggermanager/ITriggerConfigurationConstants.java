29c1200d08fca8ca0c18cf1c504639de6d608269
Morris Guan <44405167+mkwan01@users.noreply.github.com>
2024-08-23 11:02:15 -0400

    Remove comments on default values

--- src/main/java/io/github/theprez/triggermanager/ITriggerConfigurationConstants.java
+++ src/main/java/io/github/theprez/triggermanager/ITriggerConfigurationConstants.java
@@ -2,11 +2,9 @@ package io.github.theprez.triggermanager;
 
 public interface ITriggerConfigurationConstants {
     // The library where the triggers, variables, and data queues are saved
-    // Default value: TRIGGERMAN
     static final String KEY_TRIGGER_MANAGER_LIBRARY = "TRIGGER_MANAGER_LIBRARY";
 
     // The Kafka broker uri
-    // Default value: idevphp.idevcloud.com:9092
     static final String KEY_KAFKA_BROKER_URI = "KAFKA_BROKER_URI";
 
     // Default path of AIStream configuration file on IBM i host
