9fdfef696fb8a7634020360282b0bd63f350d1b2
Morris Guan <44405167+mkwan01@users.noreply.github.com>
2025-02-27 10:15:36 -0500

    New config constants
    New constants for aistream root path and stop daemon script.

--- src/main/java/io/github/theprez/triggermanager/ITriggerConfigurationConstants.java
+++ src/main/java/io/github/theprez/triggermanager/ITriggerConfigurationConstants.java
@@ -7,8 +7,14 @@ interface ITriggerConfigurationConstants {
     // The Kafka broker uri
     static final String KEY_KAFKA_BROKER_URI = "KAFKA_BROKER_URI";
 
+    // The root path for AIStream on IBM i
+    static final String AISTREAM_ROOT_PATH = "/QOpenSys/QIBM/ProdData/aistream";
+
     // Default path of AIStream configuration file on IBM i host
-    static final String DEFAULT_CONFIG_PATH = "/QOpenSys/etc/aistream/main.conf";
+    static final String DEFAULT_CONFIG_PATH = AISTREAM_ROOT_PATH + "/etc/main.conf";
+
+    // Fully qualified path of the stop daemon script
+    static final String STOP_DAEMON_SCRIPT_PATH = AISTREAM_ROOT_PATH + "/bin/stopDaemon.sh";
 
     // Environment variable to override the path of the AIStream configuration file
     static final String ENV_AISTREAM_CONFIG_FILE = "AISTREAM_CONFIG_FILE";
