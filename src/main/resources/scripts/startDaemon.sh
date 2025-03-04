0a55c822240e93f9f29abcc6c5dd458232d4ba81
Morris Guan <44405167+mkwan01@users.noreply.github.com>
2025-03-04 13:21:49 -0500

    Better code formatting

--- src/main/resources/scripts/startDaemon.sh
+++ src/main/resources/scripts/startDaemon.sh
@@ -8,14 +8,14 @@ aistream_jar=`find ${PARENT_PATH}/../aistream*.jar`
 
 # Exit from the script if the jar file is not found.
 if [ -z "${aistream_jar}" ]; then	
-  echo "AIStream jar is not found."  
-  exit 1
+    echo "AIStream jar is not found."  
+    exit 1
 fi
 
 # If JAVA_HOME is set, then use the java command under $JAVA_HOME/bin, otherwise just use "java".
 java_command="java"
 if [ -n "${JAVA_HOME}" ]; then
-	java_command=${JAVA_HOME}/bin/java
+    java_command=${JAVA_HOME}/bin/java
 fi
 
 exec ${java_command} -jar ${aistream_jar} --action DAEMONSTART -v
